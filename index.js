a811192c62f7eb45fd6a1d7288b41db66e6811e6
Ravindra Prasad <ravindraprasad@yahoo.com>
2017-03-03 15:01:40 +0530

    Add files via upload
    This is for chatbot test

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require ('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application 
+app.use(bodyParser.urlencoded({extended: false}))
+
+//Process application / JSON
+
+app.use(bodyParser.json())
+
+//Index route
+app.get('/', function(req,res) {
+    res.send('Hello world, I am Lisa, your friendly chatbot')
+})
+
+
+//for facebook verification
+
+app.get('/webhook/', function (req,res) {
+    if (req.query['hub.verify_token'] === 'any password you like') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token please')
+})
+
+// Spin up the server
+
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+
